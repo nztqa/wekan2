d1d0c32f5983dc01504f872045633ec1b2f12f10
nztqa <nztqa@users.noreply.github.com>
2017-12-22 16:11:39 +0900

    Add some translation templates

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import AppLayout from '../../ui/layouts/AppLayout.js';
+import './locale.js';
 
 Meteor.startup(() => {
   render(<AppLayout />, document.getElementById('react-root'));
